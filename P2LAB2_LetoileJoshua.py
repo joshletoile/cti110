9aeb2cf2252eab7e294cc6f6b82f6e863e98c8dd
joshletoile <113452161+joshletoile@users.noreply.github.com>
2022-09-29 11:08:38 -0400

    Add files via upload

---
+++ P2LAB2_LetoileJoshua.py
@@ -0,0 +1,10 @@
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+
+product = num1 * num2 * num3 * num4
+avg = (num1 + num2 + num3 + num4) / 4
+
+print(f'{product:.0f} {avg:.0f}')
+print(f'{product:.3f} {avg:.3f}')
